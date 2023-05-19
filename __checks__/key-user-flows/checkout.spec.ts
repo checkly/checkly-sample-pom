44b933ea1e36f9f01378ff9119e102d8e7a8c3e6
ragog <giovanni@checklyhq.com>
2023-05-19 15:50:29 +0200

    refactor: clean up stale config

--- __checks__/key-user-flows/checkout.spec.ts
+++ __checks__/key-user-flows/checkout.spec.ts
@@ -5,13 +5,9 @@ import { DanubeCartPage } from './pages/cart';
 import { DanubeCheckoutPage } from './pages/checkout';
 
 test('checkout', async ({ page }) => {
-	const navigationPromise = page.waitForNavigation();
 
-	// navigate to our target web page
 	await page.goto('https://danube-web.shop');
 
-	// add the first item to the cart
-	// await page.click(`.preview:nth-child(1) > .preview-author`);
 	const shopPage = new DanubeShopPage(page);
 	const itemDetailsPage = new DanubeItemDetailsPage(page);
 	const cartPage = new DanubeCartPage(page);
@@ -19,12 +15,6 @@ test('checkout', async ({ page }) => {
 
 	await shopPage.selectShopItem();
 	await itemDetailsPage.addToCart();
-	// await shopPage.goto();
-
-	// wait until navigation is complete
-	await navigationPromise;
-
-	// navigate to cart and proceed
 	await cartPage.proceedToCheckout();
 	await checkoutPage.completeOrderForm();
 	await checkoutPage.finalizeOrder();
