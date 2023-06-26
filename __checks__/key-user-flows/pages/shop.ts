cbad36f93d472a05aa7959340abb9afa2f803a85
ragog <giovanni@checklyhq.com>
2023-06-26 13:40:56 +0200

    feat: search and login scenarios

--- __checks__/key-user-flows/pages/shop.ts
+++ __checks__/key-user-flows/pages/shop.ts
@@ -4,15 +4,25 @@ export class DanubeShopPage {
 	readonly page: Page;
 	readonly shopItems: Locator;
 	readonly shopSearchBar: Locator;
-    readonly shopSearchButton: Locator;
-    readonly logo: Locator;
+	readonly shopSearchButton: Locator;
+	readonly buttonLogin: Locator;
+	readonly fieldLoginEmail: Locator;
+	readonly fieldLoginPassword: Locator;
+	readonly buttonLoginProceed: Locator;
+	readonly loginMessage: Locator;
+	readonly logo: Locator;
 
 	constructor(page: Page) {
 		this.page = page;
 		this.shopItems = page.locator('.preview > .preview-author');
 		this.shopSearchBar = page.locator('input');
 		this.shopSearchButton = page.locator('#button-search');
-        this.logo = page.locator('#logo')
+		this.buttonLogin = page.locator('#login');
+		this.fieldLoginEmail = page.locator('#n-email');
+		this.fieldLoginPassword = page.locator('#n-password2');
+		this.buttonLoginProceed = page.locator('#goto-signin-btn');
+		this.loginMessage = page.locator('#login-message');
+		this.logo = page.locator('#logo');
 	}
 
 	async selectShopItem() {
@@ -21,10 +31,21 @@ export class DanubeShopPage {
 
 	async performSearch(searchString: string) {
 		await this.shopSearchBar.fill(searchString);
-        await this.shopSearchButton.click();
+		await this.shopSearchButton.click();
 	}
 
-    async goto() {
-        await this.logo.click()
-    }
+	async performLogin(user) {
+		await this.buttonLogin.click();
+		await this.fieldLoginEmail.fill(user.email);
+		await this.fieldLoginPassword.fill(user.password);
+		await this.buttonLoginProceed.click();
+	}
+
+	async isPastLogin() {
+		return this.loginMessage.isVisible();
+	}
+
+	async goto() {
+		await this.logo.click();
+	}
 }
